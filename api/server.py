e011d43f6a51438d54e4be2da23cec84e09262ee
kushaal-rana <email@example.com>
2023-08-18 00:15:51 -0400

    add cors

--- api/server.py
+++ api/server.py
@@ -9,8 +9,8 @@ import io
 import zipfile
 
 app = Flask(__name__)
-cors = CORS(app, resources={r'*':{'origins': '*'}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app, resources={r'*':{'origins': '*'}})
+# app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/execute-script', methods=['POST', 'OPTIONS'])
 @cross_origin()
@@ -76,6 +76,7 @@ def execute_script():
 
 
 @app.route('/get-csv/<filename>', methods=['GET'])
+@cross_origin()
 def get_csv(filename):
     try:
         csv_filepath = os.path.join(request.args.get('download_path', ''), filename)
